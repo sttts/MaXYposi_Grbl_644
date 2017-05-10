1f0bf3b9575f31bb6cfed25a3900729a80ed2d63
heise <cm@ct.de>
2017-05-10 11:14:58 +0200

    hints added

--- grblUpload_644/grblUpload_644.ino
+++ grblUpload_644/grblUpload_644.ino
@@ -1,18 +1,36 @@
 /***************************************************************************
+
+Dieser Sketch kompiliert GRBL-644 und lädt es auf die MaXYposi-Platine. Bitte 
+richten Sie die Arduino-IDE folgendermaßen ein:
+
+- Verschieben Sie das Verzeichnis "grblUpload_644" vom Github-Download in Ihren "Arduino"-Sketch-Ordner.
+- Verschieben Sie das Verzeichnis "hardware" vom Github-Download in Ihren "Arduino"-Sketch-Ordner.
+- Verschieben Sie das Verzeichnis "libraries\grbl_644" vom Github-Download nach "Arduino\libraries\grbl_644".
+- Öffnen Sie den Sketch "..\Arduino\grblUpload_644\grblUpload_644.ino"
+- Wählen Sie im Menü "Werkzeuge:Board" den Eintrag "MaXYposi".
+- Wählen Sie im Menü "Werkzeuge:Prozessor" den Eintrag "ATmega644A" oder "ATmega644P", je nachdem, welche Variante Sie verwenden.
+
+Sie können nun GRBL mit der Arduino-IDE kompilieren und auch auf das Board 
+hochladen; die mit der Platine gelieferten prozessoren sind bereits mit einem 
+Sanguino-Optiboot-kompatiblen Bootloader ausgestattet.
+
+****************************************************************************
+
 This sketch compiles and uploads Grbl_644 to your 644p-based MaXYposi Board!
-Install "Sanguino" booard addition from  
-https://raw.githubusercontent.com/Lauszus/Sanguino/master/package_lauszus_sanguino_index.json
 
 GrblUpload changed by -cm to explicitely use grbl_644 library
 
 To use:
 - Copy enclosed "hardware" directory into your Arduino directory.
 - Copy entire "grbl_644" directory into "libraries" inside your Arduino directory.
-- Select "MaXYiposi" board and FTDI device's Serial Port in the Tools drop-down menu.
+- Select "MaXYiposi" board.
+- Check if your CPU is ATmega644A or ATmega644P. Select CPU menu accordingly.
+- FTDI device's Serial Port in the Tools drop-down menu.
 
 - Then just click 'Upload'. That's it!
 
 For advanced users:
+
   If you'd like to see what else Grbl can do, there are some additional
   options for customization and features you can enable or disable. 
   Navigate your file system to where the Arduino IDE has stored the Grbl 
